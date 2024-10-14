558e52a503c28f80fa018b3441b180133935902a
jangwonik <dhslrl321@gmail.com>
2024-10-14 21:17:45 +0900

    commit

---
+++ src/main/kotlin/com/github/dhslrl321/bulkqueryapi/BulkQueryApiApplication.kt
@@ -0,0 +1,11 @@
+package com.github.dhslrl321.bulkqueryapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BulkQueryApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<BulkQueryApiApplication>(*args)
+}
